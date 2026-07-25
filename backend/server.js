3ec4a4f240bfc18a72a825e681520e75f957ee97
Anchal1808 <anchaldwiedi015@gmail.com>
2026-07-25 13:29:37 +0530

    Implemented user registration API

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,12 @@
+const authRoutes = require("./routes/authRoutes");
 const express = require("express");
 require("dotenv").config();
-
 const connectDB = require("./config/db");
-
 const app = express();
-
 const PORT = 5000;
-
 app.use(express.json());
-
-
+app.use("/api/auth", authRoutes);
 connectDB();
-
-
 app.get("/", (req,res)=>{
     res.send("Expense Tracker Backend Running");
 });
